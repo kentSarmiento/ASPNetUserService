7a8964eaa96400d4eb33bed35704e8a6c3efb9ef
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-02-19 15:11:34 +0800

    Initial commit for User Authentication Project

---
+++ src/ASPNetUserService.API/Controllers/ResourceController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using ASPNetUserService.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Validation.AspNetCore;
+
+namespace ASPNetUserService.API.Controllers
+{
+    [Route("api")]
+    [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+    public class ResourceController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ResourceController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("message")]
+        public async Task<IActionResult> GetMessage()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            return Content($"{user.UserName} has been successfully authenticated.");
+        }
+    }
+}
---
+++ test/ASPNetUserService.TestClient/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using OpenIddict.Abstractions;
+
+namespace ASPNetUserService.TestClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+
+            const string email = "bob@le-magnifique.com", password = "}s>EWG@f4g;_v7nB";
+
+            await CreateAccountAsync(client, email, password);
+
+            var token = await GetTokenAsync(client, email, password);
+            Console.WriteLine("Access token: {0}", token);
+            Console.WriteLine();
+
+            var resource = await GetResourceAsync(client, token);
+            Console.WriteLine("API response: {0}", resource);
+
+            Console.ReadLine();
+        }
+
+        public static async Task CreateAccountAsync(HttpClient client, string email, string password)
+        {
+            var response = await client.PostAsJsonAsync("https://localhost:5001/Account/Register", new { email, password });
+
+            // Ignore 409 responses, as they indicate that the account already exists.
+            if (response.StatusCode == HttpStatusCode.Conflict)
+            {
+                return;
+            }
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task<string> GetTokenAsync(HttpClient client, string email, string password)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:5001/connect/token");
+            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                ["grant_type"] = "password",
+                ["username"] = email,
+                ["password"] = password
+            });
+
+            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+
+            var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
+
+            if (!string.IsNullOrEmpty(payload.Error))
+            {
+                throw new InvalidOperationException("An error occurred while retrieving an access token.");
+            }
+
+            return payload.AccessToken;
+        }
+
+        public static async Task<string> GetResourceAsync(HttpClient client, string token)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001/api/message");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
