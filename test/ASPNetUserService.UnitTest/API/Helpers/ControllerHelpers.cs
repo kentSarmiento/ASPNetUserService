ab145c3db459d418d85436fcca929e8dc00394fb
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-02-20 14:43:42 +0800

    Initial Unit Test Project

---
+++ test/ASPNetUserService.UnitTest/API/Helpers/ControllerHelpers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
+
+namespace ASPNetUserService.UnitTest.API.Helpers
+{
+    public class ControllerHelpers
+    {
+        public static ControllerContext GetControllerContext(HttpContext httpContext)
+        {
+            return new ControllerContext(new ActionContext(httpContext, new RouteData(), new ControllerActionDescriptor()));
+        }
+    }
+}
