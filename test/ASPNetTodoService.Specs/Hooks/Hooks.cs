05e8d5d086a28afcfa74f383043fc382c21ed32e
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-02-20 16:40:02 +0800

    Initial Spec Test Project

---
+++ test/ASPNetTodoService.Specs/Hooks/Hooks.cs
@@ -0,0 +1,25 @@
+﻿using ASPNetUserService.API;
+using Microsoft.Extensions.Hosting;
+using TechTalk.SpecFlow;
+
+namespace ASPNetTodoService.Specs.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        private static IHost? _host;
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            _host = Program.CreateHostBuilder(null).Build();
+            _host.StartAsync().Wait();
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            _host?.StopAsync().Wait();
+        }
+    }
+}
